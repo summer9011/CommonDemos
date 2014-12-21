9df2cabb3512aea85981ce55af67f35a01c98688
summer9011 <zhao_li_bo@163.com>
2014-12-21 21:27:30 +0800

    修改

--- BaiduAPI/ClientController.m
+++ BaiduAPI/ClientController.m
@@ -9,8 +9,9 @@
 #import "ClientController.h"
 #import "AsyncSocket.h"
 
-@interface ClientController () {
+@interface ClientController () <AsyncSocketDelegate,UITableViewDataSource,UITableViewDelegate> {
     NSString *receiverID;           //接收者的ID
+    AsyncSocket *asyncSocket;
 }
 
 @property (weak, nonatomic) IBOutlet UITextField *ipTF;
@@ -29,19 +30,99 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    _getListBtn.enabled=NO;
+    _sendBtn.enabled=NO;
+    
+    [_receiverList registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
+    
+    asyncSocket=[[AsyncSocket alloc] initWithDelegate:self];
+    
 }
 
 //Do connect to socketServer
 - (IBAction)doConnect:(id)sender {
-    
+    if (_ipTF.text&&_portTF.text) {
+        unsigned short port=[_portTF.text intValue];
+        
+        NSError *error;
+        if (![asyncSocket connectToHost:_ipTF.text onPort:port error:&error]) {
+            NSLog(@"Connect Error %@",error);
+        }else{
+            _connectBtn.enabled=NO;
+            _getListBtn.enabled=YES;
+        }
+    }
 }
 
 //Do get available receiver list
 - (IBAction)doGetList:(id)sender {
+    
 }
 
 //Do send data to socketServer
 - (IBAction)doSendData:(id)sender {
+    
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell=[tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    //conf the cell
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+#pragma mark - AsyncSocketDelegate
+
+- (void)onSocketDidDisconnect:(AsyncSocket *)sock {
+    NSLog(@"onSocketDidDisconnect:");
+}
+
+- (void)onSocket:(AsyncSocket *)sock didAcceptNewSocket:(AsyncSocket *)newSocket {
+    NSLog(@"onSocket:didAcceptNewSocket:");
+}
+
+- (NSRunLoop *)onSocket:(AsyncSocket *)sock wantsRunLoopForNewSocket:(AsyncSocket *)newSocket {
+    NSLog(@"onSocket:wantsRunLoopForNewSocket:");
+    
+    return [NSRunLoop currentRunLoop];
+}
+
+- (void)onSocket:(AsyncSocket *)sock didConnectToHost:(NSString *)host port:(UInt16)port {
+    NSLog(@"onSocket:didConnectToHost:%@ port:%d",host,port);
+}
+
+- (void)onSocket:(AsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag {
+    NSLog(@"onSocket:didReadData:withTag:");
+}
+
+- (void)onSocket:(AsyncSocket *)sock didReadPartialDataOfLength:(NSUInteger)partialLength tag:(long)tag {
+    NSLog(@"onSocket:didReadPartialDataOfLength:tag:");
+}
+
+- (void)onSocket:(AsyncSocket *)sock didWriteDataWithTag:(long)tag {
+    NSLog(@"onSocket:didWriteDataWithTag:");
+}
+
+- (void)onSocket:(AsyncSocket *)sock didWritePartialDataOfLength:(NSUInteger)partialLength tag:(long)tag {
+    NSLog(@"onSocket:didWritePartialDataOfLength:tag:");
+}
+
+- (void)onSocketDidSecure:(AsyncSocket *)sock {
+    NSLog(@"onSocketDidSecure:");
 }
 
 @end
